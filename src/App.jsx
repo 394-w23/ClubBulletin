32b5af9c5c6ff8274e754ded30be2a6688b2cc0c
Zarif Ceaser <zarifceaser321@gmail.com>
2023-01-05 20:41:10 -0600

    trying to display multiple posts

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,23 @@ import "./App.css";
 
 const App = () => {
   const [count, setCount] = useState(0);
-  const [data, error] = useDbData("/");
+  const [data, error] = useDbData("/"); // get whole database
 
   if (error) return <h1>Error loading data: {error.toString()}</h1>;
   if (data === undefined) return <h1>Loading data...</h1>;
   if (!data) return <h1>No data found</h1>;
 
+  const currentUser = data.users["27e416aa-8d61-11ed-a1eb-0242ac120002"];
+  const currentClubs = Object.entries(currentUser.clubs);
+  const postList = Object.entries(data.posts);
+  //console.log(postList);
+  console.log(postList.filter(([id, value]) => currentClubs.includes(value.clubId)));
   return (
-    <div className="App">
-      {<Post post={data.posts["7a5e975c-8d61-11ed-a1eb-0242ac120002"]}></Post>}
+    // <div className="App">
+    //   {<Post post={}></Post>}
+    // </div>
+    <div>
+
     </div>
   );
 };
