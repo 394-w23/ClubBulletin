a7a350737881a51b6c6b105be270730d32a1785b
alfenn <52635707+alfenn@users.noreply.github.com>
2023-01-05 19:41:15 -0600

    add firebase config

---
+++ src/utilities/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQZqOv4h0oW2NPYvj6xLvnrzH5_CH-NgQ",
+  authDomain: "clubbulletin-e6cf8.firebaseapp.com",
+  databaseURL: "https://clubbulletin-e6cf8-default-rtdb.firebaseio.com",
+  projectId: "clubbulletin-e6cf8",
+  storageBucket: "clubbulletin-e6cf8.appspot.com",
+  messagingSenderId: "128903378781",
+  appId: "1:128903378781:web:f9945c665b876759689ab4"
+};
+
+const firebase = initializeApp(firebaseConfig);
