86d2bd961fc3cdc4d3c682eb663a91ba85c0ac1e
alfenn <52635707+alfenn@users.noreply.github.com>
2023-01-08 22:35:25 -0600

    fix selection, fix styling of selector

--- src/components/ClubSelector/ClubSelector.jsx
+++ src/components/ClubSelector/ClubSelector.jsx
@@ -2,37 +2,38 @@ const ClubButton = ({ club, id, selection, setSelection }) => {
   //console.log("club", club);
   //console.log("id", id);
   return (
-  <div>
-    <input
-      type="radio"
-      id={club}
-      checked={club === selection}
-      autoComplete="off"
-      onChange={() => setSelection(id)}
-    />
-    <label htmlFor={club.name}>
-      {club.name}
-    </label>
-  </div>
-);
+    <div>
+      <input
+        type="radio"
+        className="btn-check"
+        id={id}
+        checked={id === selection}
+        autoComplete="off"
+        onChange={() => setSelection(id)}
+      />
+      <label htmlFor={id} className="btn btn-success mb-1 p-2">
+        {club.name}
+      </label>
+    </div>
+  );
 }
 
 const ClubSelector = ({ clubs, selection, setSelection }) => {
   //console.log(clubs);
-  //console.log("selection", selection);
+  console.log("selection", selection);
   return (
-  <div className="btn-group">
-    {clubs.map(([id, club]) => (
-      <ClubButton
-        id={id}
-        key={id}
-        club={club}
-        selection={selection}
-        setSelection={setSelection}
-      />
-    ))}
-  </div>
-);
-    }
+    <div className="btn-group">
+      {clubs.map(([id, club]) => (
+        <ClubButton
+          id={id}
+          key={id}
+          club={club}
+          selection={selection}
+          setSelection={setSelection}
+        />
+      ))}
+    </div>
+  );
+}
 
 export default ClubSelector;
--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -4,31 +4,8 @@ import Post from "../components/Post/Post";
 import ClubSelector from "../components/ClubSelector/ClubSelector";
 
 const Feed = () => {
-  const [count, setCount] = useState(0);
   const [data, error] = useDbData("/"); // get whole database
-
-  const initialValue =
-    error || data === undefined
-      ? 0
-      : Object.entries(data.clubs).filter(([id, value]) =>
-          Object.values(
-            data.users["27e416aa-8d61-11ed-a1eb-0242ac120002"].clubs
-          ).includes(id)
-        )[0][0];
-
-  const [selection, setSelection] = useState(data);
-  
-  useEffect(() => {
-    console.log("useEffect")
-    if (selection !== undefined){
-      console.log("not undefined", selection)
-      setSelection(selection);
-    } else{
-      console.log("initalValue")
-      setSelection(initialValue);
-    }
-
-  });
+  const [selection, setSelection] = useState("all");  
 
   // console.log("selection:", selection);
   // console.log("initial value", initialValue);
@@ -37,22 +14,12 @@ const Feed = () => {
   if (data === undefined) return <h1>Loading data...</h1>;
   if (!data) return <h1>No data found</h1>;
 
-  // console.log("checking entires",Object.entries(data.clubs));
   const currentUser = data.users["27e416aa-8d61-11ed-a1eb-0242ac120002"];
   const currentClubsIds = Object.values(currentUser.clubs);
   const currentClubs = Object.entries(data.clubs).filter(([id, value]) =>
     currentClubsIds.includes(id)
   );
-  const allPosts = Object.entries(data.posts);
-  // console.log(currentClubs[0][0]);
-
-  // console.log("current clubs ids", currentClubsIds);
-  // console.log("current clubs", currentClubs);
-  // console.log("post", allPosts);
-  // console.log(
-  //   "filter",
-  //   allPosts.filter(([id, value]) => currentClubsIds.includes(value.clubId))
-  // );
+  const allPosts = Object.entries(data.posts);  
   const filteredPosts = allPosts.filter(([id, value]) =>
     currentClubsIds.includes(value.clubId)
   );
