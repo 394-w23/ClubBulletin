ca95989690b151c520e043e038b90e44442496fc
Kevin Rueda <rueda.kv@gmail.com>
2023-01-08 21:21:44 -0600

    debug club radio button

--- src/components/ClubSelector/ClubSelector.jsx
+++ src/components/ClubSelector/ClubSelector.jsx
@@ -1,21 +1,30 @@
-
-const ClubButton = ({club}) => (
+const ClubButton = ({ club, selection, setSelection }) => (
   <div>
-    <input type="radio" id={meal} className="btn-check" checked={club === selection} autoComplete="off"
-      onChange={() => setSelection(club)} />
+    <input
+      type="radio"
+      id={club}
+      className="btn-check"
+      checked={club === selection}
+      autoComplete="off"
+      onChange={() => setSelection(club)}
+    />
     <label className="btn btn-success mb-1 p-2" htmlFor={club}>
-    { club }
+      {club}
     </label>
   </div>
 );
 
-const ClubSelector = ({clubs, selection, setSelection}) => (
+const ClubSelector = ({ clubs, selection, setSelection }) => (
   <div className="btn-group">
-    { 
-      Object.keys(clubs).map(club => <ClubButton key={club} club={club} selection={selection} setSelection={setSelection} />)
-    }
+    {Object.keys(clubs).map((club) => (
+      <ClubButton
+        key={club}
+        club={club}
+        selection={selection}
+        setSelection={setSelection}
+      />
+    ))}
   </div>
 );
-
 
 export default ClubSelector;
--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -4,13 +4,31 @@ import Post from "../components/Post/Post";
 import ClubSelector from "../components/ClubSelector/ClubSelector";
 
 const Feed = () => {
-  const [count, setCount] = useState(0);  
+  const [count, setCount] = useState(0);
   const [data, error] = useDbData("/"); // get whole database
-  const [selection, setSelection] = useState(0);
+
+  const initialValue =
+    error || data === undefined
+      ? 0
+      : Object.entries(data.clubs).filter(([id, value]) =>
+          Object.values(
+            data.users["27e416aa-8d61-11ed-a1eb-0242ac120002"].clubs
+          ).includes(id)
+        )[0][0];
+
+  const [selection, setSelection] = useState(data);
+
+  useEffect(() => {
+    setSelection(initialValue);
+  });
+
+  console.log("selection:", selection);
+  console.log("initial value", initialValue);
 
   if (error) return <h1>Error loading data: {error.toString()}</h1>;
   if (data === undefined) return <h1>Loading data...</h1>;
   if (!data) return <h1>No data found</h1>;
+
   // console.log("checking entires",Object.entries(data.clubs));
   const currentUser = data.users["27e416aa-8d61-11ed-a1eb-0242ac120002"];
   const currentClubsIds = Object.values(currentUser.clubs);
@@ -18,10 +36,7 @@ const Feed = () => {
     currentClubsIds.includes(id)
   );
   const allPosts = Object.entries(data.posts);
-  console.log(currentClubs[0][0]);
-  useEffect(() => setSelection(currentClubs[0][0]));
-  
-
+  // console.log(currentClubs[0][0]);
 
   // console.log("current clubs ids", currentClubsIds);
   // console.log("current clubs", currentClubs);
@@ -31,13 +46,17 @@ const Feed = () => {
   //   allPosts.filter(([id, value]) => currentClubsIds.includes(value.clubId))
   // );
   const filteredPosts = allPosts.filter(([id, value]) =>
-  currentClubsIds.includes(value.clubId)
+    currentClubsIds.includes(value.clubId)
   );
   // something something postId to delete posts later (warning)
-  
+
   return (
     <div className="App">
-      {/* <ClubSelector clubs={currentClubs} selection={selection} setSelection={setSelection} /> */}
+      <ClubSelector
+        clubs={currentClubs}
+        selection={selection}
+        setSelection={setSelection}
+      />
       {filteredPosts.map(([id, post]) => (
         <Post key={id} post={post} />
       ))}
@@ -46,4 +65,3 @@ const Feed = () => {
 };
 
 export default Feed;
-
