011ce6bd66394b3cb20d578c08d59a652d209981
Nicholas Tsung <nicktsung@gmail.com>
2023-01-15 21:31:17 -0600

    Add more fields to new club form

--- src/pages/NewClub.jsx
+++ src/pages/NewClub.jsx
@@ -6,18 +6,27 @@ import Container from "react-bootstrap/Container";
 function NewClub({ data, currentUserId, currentUserData }) {
   const {...allUsers} = data.users;
   console.log(allUsers);
+  const handleSubmit = (event) => {
+    console.log(event);
+    event.preventDefault();
+    const form = event.currentTarget;
+  };
   return (
     <Container>
       <Navigation currentUserData={currentUserData} />
       <Button varient="primary" href="/organizations">Back</Button>
-      <Form>
+      <Form onSubmit={handleSubmit}>
       <Form.Label>Club Admin</Form.Label>
         <Form.Select aria-label="Club Admins"> 
           <option>Choose admin</option>
-          {allUsers.map(([id, user]) => {
-              return <option value={id}>{user.name}</option>;
+          {Object.keys(allUsers).map((key,index) => {
+              return <option key={key}>{allUsers[key].name}</option>;
             })}
         </Form.Select>
+      <Form.Label>Club Name</Form.Label>
+        <Form.Control></Form.Control><Form.Text id="ClubName"></Form.Text>
+      <Form.Label>Club Description</Form.Label>
+        <Form.Control></Form.Control><Form.Text id="ClubDescription"></Form.Text>
         <Button variant="primary" type="submit">
           Submit
         </Button>
