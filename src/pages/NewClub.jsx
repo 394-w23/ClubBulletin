e1aa25d8cc9c1a8dc3ca956b61a3a7cb5afee5a1
mayablumovitz <63746979+mayablumovitz@users.noreply.github.com>
2023-01-15 21:49:22 -0600

    Create handleSubmit on NewClub form

--- src/pages/NewClub.jsx
+++ src/pages/NewClub.jsx
@@ -2,31 +2,41 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Navigation from "../components/Navigation/Navigation";
 import Container from "react-bootstrap/Container";
+import { useDbUpdate } from "../../utilities/firebase";
 
 function NewClub({ data, currentUserId, currentUserData }) {
-  const {...allUsers} = data.users;
-  console.log(allUsers);
+  const { ...allUsers } = data.users;
+
   const handleSubmit = (event) => {
-    console.log(event);
     event.preventDefault();
-    const form = event.currentTarget;
+    const formData = new FormData(event.target);
+    const formDataObj = Object.fromEntries(formData.entries());
+    console.log(formDataObj);
+
+    const [updateClub] = useDbUpdate(`/clubs`);
+
   };
+
   return (
     <Container>
       <Navigation currentUserData={currentUserData} />
       <Button varient="primary" href="/organizations">Back</Button>
+
       <Form onSubmit={handleSubmit}>
-      <Form.Label>Club Admin</Form.Label>
-        <Form.Select aria-label="Club Admins"> 
+        <Form.Label>Club Admin</Form.Label>
+        <Form.Select aria-label="Club Admins" name="ClubAdmin">
           <option>Choose admin</option>
-          {Object.keys(allUsers).map((key,index) => {
-              return <option key={key}>{allUsers[key].name}</option>;
-            })}
+          {Object.keys(allUsers).map((key, index) => {
+            return <option key={key}>{allUsers[key].name}</option>;
+          })}
         </Form.Select>
-      <Form.Label>Club Name</Form.Label>
-        <Form.Control></Form.Control><Form.Text id="ClubName"></Form.Text>
-      <Form.Label>Club Description</Form.Label>
-        <Form.Control></Form.Control><Form.Text id="ClubDescription"></Form.Text>
+
+        <Form.Label>Club Name</Form.Label>
+        <Form.Control type="text" name="ClubName"></Form.Control>
+
+        <Form.Label>Club Description</Form.Label>
+        <Form.Control type="text" name="ClubDescription"></Form.Control>
+
         <Button variant="primary" type="submit">
           Submit
         </Button>
