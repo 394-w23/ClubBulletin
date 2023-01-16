2be0d8dff4eafb778795d365215e5db140933282
mayablumovitz <63746979+mayablumovitz@users.noreply.github.com>
2023-01-15 22:27:31 -0600

    Updates to NewClub to post to FireBase

--- src/pages/NewClub.jsx
+++ src/pages/NewClub.jsx
@@ -7,52 +7,56 @@ import { v4 as uuidv4 } from 'uuid';
 
 function NewClub({ data, currentUserId, currentUserData }) {
   const { ...allUsers } = data.users;
+  const [update] = useDbUpdate(`/`);
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
     const formDataObj = Object.fromEntries(formData.entries());
-    console.log(formDataObj);
 
     // update /clubs with a new club 
-    const [update] = useDbUpdate(`/`);
-    update(['/clubs']: {...data.clubs, 
-      `${uuidv4()}`}: 
+    const newid = uuidv4();
+
+    update({['/clubs']: { ...data.clubs, 
+      [newid]:
+      {
         'description': formDataObj.ClubDescription,
         'admins': ['', currentUserId],
         'name': formDataObj.ClubName,
-        'members': [''])
-    // TODO: update /users.<adminId>.clubs with the new club 
+        'members': ['']
+      }}})
+  // TODO: display success to user
+  // TODO: update /users.<adminId>.clubs with the new club 
 
-  };
+};
 
-  return (
-    <Container>
-      <Navigation currentUserData={currentUserData} />
-      <Button varient="primary" href="/organizations">Back</Button>
+return (
+  <Container>
+    <Navigation currentUserData={currentUserData} />
+    <Button varient="primary" href="/organizations">Back</Button>
 
-      <Form onSubmit={handleSubmit}>
-        <Form.Label>Club Admin</Form.Label>
+    <Form onSubmit={handleSubmit}>
+      {/* <Form.Label>Club Admin</Form.Label>
         <Form.Select aria-label="Club Admins" name="ClubAdmin">
           <option>Choose admin</option>
           {Object.keys(allUsers).map((key, index) => {
             return <option key={key}>{allUsers[key].name}</option>;
           })}
-        </Form.Select>
+        </Form.Select> */}
 
-        <Form.Label>Club Name</Form.Label>
-        <Form.Control type="text" name="ClubName"></Form.Control>
+      <Form.Label>Club Name</Form.Label>
+      <Form.Control type="text" name="ClubName"></Form.Control>
 
-        <Form.Label>Club Description</Form.Label>
-        <Form.Control type="text" name="ClubDescription"></Form.Control>
+      <Form.Label>Club Description</Form.Label>
+      <Form.Control type="text" name="ClubDescription"></Form.Control>
 
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-    </Container>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  </Container>
 
-  );
+);
 }
 
 export default NewClub;
