0561492a9486dc55167b2856369deb5383015d69
Chris Naing <clnaing23@gmail.com>
2023-01-17 13:57:35 -0600

    login page styling

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,7 +1,7 @@
 import { NavLink } from "react-router-dom";
 import { signInWithGoogle, signOut, useAuthState } from "../utilities/firebase";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import "../styles/logIn.css";
 const SignInButton = () => (
   <button className="ms-auto btn btn-dark" onClick={signInWithGoogle}>
     Sign in
@@ -22,13 +22,11 @@ const AuthButton = () => {
 const activation = ({ isActive }) => (isActive ? "active" : "inactive");
 
 const LogIn = () => (
-  <div>
-    <nav className="d-flex">
-      <AuthButton />
-    </nav>
-    <div>
+  <div className="main">
+    <div className="text">
       <h1>Club Bulletin</h1>
       <p>Sign in to view posts and join clubs.</p>
+      <AuthButton />
     </div>
   </div>
 );
